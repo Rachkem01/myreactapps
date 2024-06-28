1f211bd0ecd1b4bd65d00fc19b91979d164ddd50
Rachael <agunlokokemi01@gmail.com>
2024-06-28 14:27:53 +0100

    changes

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -11,7 +11,7 @@ return <>
     <div className='logo-container'>
         <h1>
             {/* <FiShoppingCart /> */}
-            <img src={AkubeDark} style={{width:150, height:150}} className='Logo' alt= "logo"></img>
+            <img src={AkubeDark} style={{width:150, height:220}} className='Logo' alt= "logo"></img>
          
         </h1>
     </div>
